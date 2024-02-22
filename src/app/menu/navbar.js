e2c5ee72496c5439885316c36b0d53979732012e
Tristan H <tristanskyhook@gmail.com>
2024-02-22 11:30:20 -0800

    nav

---
+++ src/app/menu/navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar() {
+    return (
+        <nav>
+        <ul>
+            <li>
+            <a href="/">Home</a>
+            </li>
+            <li>
+            <a href="/about">About</a>
+            </li>
+        </ul>
+        </nav>
+    );
+    }
+
